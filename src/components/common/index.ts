e106b5b42c81ef69f03dcc6a67411d9352df5112
Yewon <ymh20804@naver.com>
2026-07-12 00:30:57 +0900

    Fix: Button export 누락 추가

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
+export { Button } from "./Button";
 export { Divider } from "./Divider";
 export { BackHeader, DateHeader, LogoHeader } from "./Header";
 export { ScreenContainer } from "./ScreenContainer";
