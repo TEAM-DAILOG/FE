ead11b713c2a94f928293781aaba0f3b455445be
Yewon <ymh20804@naver.com>
2026-07-11 16:51:09 +0900

    Asset: 아이콘, 로고 추가 및 icons 폴더 파일명 통일

--- src/components/common/CategoryCircle.tsx
+++ src/components/common/CategoryCircle.tsx
@@ -1,6 +1,6 @@
 import { Pressable } from "react-native";
 
-import CheckIcon from "@/assets/icons/check.svg";
+import CheckIcon from "@/assets/icons/checkIcon.svg";
 import { cn } from "@/src/lib/cn";
 
 export type CategoryColor = "blue" | "brown" | "green" | "purple" | "pink";
