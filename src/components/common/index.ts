f336cb0533880813191cecc5a07fc79a63b8f20b
Yewon <ymh20804@naver.com>
2026-07-15 20:54:05 +0900

    Ui: 일정 추천 카드 컴포넌트 추가

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,5 +2,6 @@ export { Button } from "./Button";
 export { Divider } from "./Divider";
 export { BackHeader, DateHeader, LogoHeader } from "./Header";
 export { ProgressBar } from "./ProgressBar";
+export { ScheduleRecommendItem } from "./ScheduleRecommendItem";
 export { ScreenContainer } from "./ScreenContainer";
 export { Toggle } from "./Toggle";
