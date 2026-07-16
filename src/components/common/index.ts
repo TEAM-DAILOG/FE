b9ba6b317c1534a0b0e0cd6c1772927f43cbcff4
Yewon <ymh20804@naver.com>
2026-07-17 03:03:50 +0900

    Refactor: ScheduleCheckItem/ScheduleRecommendItem을 ScheduleItem으로 통합

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,8 +1,13 @@
 export { Button } from "./Button";
+export { Checkbox } from "./Checkbox";
 export { Divider } from "./Divider";
 export { BackHeader, DateHeader, LogoHeader } from "./Header";
 export { ProgressBar } from "./ProgressBar";
-export { ScheduleRecommendItem } from "./ScheduleRecommendItem";
+export {
+  ScheduleItem,
+  type ScheduleItemAction,
+  type ScheduleItemProps,
+} from "./ScheduleItem";
 export { ScreenContainer } from "./ScreenContainer";
 export { TabScrollView } from "./TabScrollView";
 export { Toggle } from "./Toggle";
