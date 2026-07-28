d8ac698bcae715ea20b01b796bf20cfeb7835487
Hyochan <hyochan02@gmail.com>
2026-07-28 22:08:55 +0900

    Refactor: 그룹핑 로직 개선

--- src/utils/groupThreadItemsByMonth.ts
+++ src/utils/groupThreadItemsByMonth.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 import { getThreadMonthKey } from "@/src/utils";
 import type {
   ThreadGroup,
@@ -6,9 +8,12 @@ import type {
 
 // 일기 스레드 아이템을 월 단위로 묶는 함수
 export function groupThreadItemsByMonth(items: ThreadItem[]): ThreadGroup[] {
+  const sortedItems = [...items].sort(
+    (a, b) => dayjs(b.date).valueOf() - dayjs(a.date).valueOf()
+  );
   const groups: ThreadGroup[] = [];
 
-  for (const item of items) {
+  for (const item of sortedItems) {
     const monthKey = getThreadMonthKey(item.date);
     const lastGroup = groups[groups.length - 1];
 
